360109bcb565d71537940c2bb1ddcbf82edb92b6
Egor Timashev <metallagetemp@gmail.com>
2018-12-19 17:33:09 +0300

    Добавьте файлы проекта.

---
+++ NSICopier/Logica.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NSICopier
+{
+    class Logica
+    {
+        //Откуда копируем
+        string sourceFiles;
+
+        //Временная папка
+        string tempPath;
+
+        //Путь к 7z 
+        string archiver;
+
+        public Logica()
+        {
+            tempPath = @"c:\temp\copyNsi\";
+            sourceFiles = @"c:\temp\fromNsi\";
+            archiver = @"C:\temp\unp\7z\7z.exe";
+
+            if(!Directory.Exists(tempPath))
+            {
+                Directory.CreateDirectory(tempPath);
+            }
+        }
+
+        public void copyToAll()
+        {
+            List<NSIDestination> destinations = new List<NSIDestination>();
+
+            NSIDestination nsiDes = new NSIDestination(@"c:\temp\nsiout\", false);
+            destinations.Add(nsiDes);
+        }
+
+
+        
+    }
+}
---
+++ NSICopier/NSIDestination.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NSICopier
+{
+    class NSIDestination
+    {
+        //Куда собственно копируем
+        string destinationPath;
+        //Нужно ли архивировать перед отправкой
+        bool needArch;
+
+
+        public NSIDestination(string destinationPath, bool needArch)
+        {
+            this.destinationPath = destinationPath;
+            this.needArch = needArch;
+        }
+
+
+        //В перспективе при неудаче она должна повторно пытаться создать директорию
+        private bool CreateOrCheckDestination()
+        {
+            bool exists = false;
+
+
+            if (Directory.Exists(destinationPath))
+            {
+                exists = true;
+            }
+            else
+            {
+                Directory.CreateDirectory(destinationPath);
+                exists = true;
+            }
+
+            return exists;
+        }
+
+        private void CopyToDestination(string file)
+        {
+            FileInfo copingFile = new FileInfo(file);
+            FileInfo destFile = new FileInfo(destinationPath + copingFile.Name);
+            copingFile.CopyTo(destFile.FullName, true);
+            //FileStream fsCopy = new FileStream(destFile.FullName, FileMode.Truncate);
+            //if(fsCopy.CanWrite)
+            //{
+            //    fsCopy.Wri
+            //}
+
+        }
+
+        /// <summary>
+        /// Нужно ли архивировать
+        /// </summary>
+        public bool NeedArch
+        { get; }
+    }
+}
